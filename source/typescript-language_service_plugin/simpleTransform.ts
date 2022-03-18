d2881a416e3779cac659d6d907bbd53fbb2ff9aa
khh <khh@omnicns.com>
2022-03-18 13:56:16 +0900

    service language plugin

---
+++ source/typescript-language_service_plugin/simpleTransform.ts
@@ -0,0 +1,7 @@
+import * as ts from "typescript";
+
+const source = "let x: string  = 'string'";
+
+let result = ts.transpileModule(source, { compilerOptions: { module: ts.ModuleKind.CommonJS }});
+
+console.log(JSON.stringify(result));
